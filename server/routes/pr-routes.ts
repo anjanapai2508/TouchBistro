dda6290296153b992dac45561a0251084a062807
Anjana Pai <anjanapai2508@gmail.com>
2023-10-02 13:17:54 -0400

    Initial test config for server side tests

--- server/routes/pr-routes.ts
+++ server/routes/pr-routes.ts
@@ -4,7 +4,7 @@ const app = Router();
 const primeNumbersController = require('../controllers/prime-numbers-controller')
 const medianController = require('../controllers/median-controller');
 app.get('/', (req: Request, res: Response) => {
-    res.send('<h1>Hello World from the Typescript inside Router</h1>')
+    res.status(200).send('Hello World from the Typescript inside Router')
 });
 app.get('/find-prime-numbers/:upperLimit', primeNumbersController);
 app.get('/find-median/:upperLimit', medianController);
