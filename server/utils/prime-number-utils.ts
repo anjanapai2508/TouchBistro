135698fc5363769e3eff136cb79eef6a79dfb7fc
Anjana Pai <anjanapai2508@gmail.com>
2023-10-03 12:09:38 -0400

    Improved variable names

--- server/utils/prime-number-utils.ts
+++ server/utils/prime-number-utils.ts
@@ -3,10 +3,10 @@ export function getPrimeNumbers(inputNumber: number): number[] {
     // ...
     const isPrime: boolean[] = new Array(inputNumber).fill(true); // Initially all numbers are assumed to be prime.
     isPrime[0] = isPrime[1] = false;
-    for (let p = 2; p * p <= inputNumber; p++) { // iterate over the array created earlier and mark
+    for (let num = 2; num * num <= inputNumber; num++) { // iterate over the array created earlier and mark
                                                  // all numbers which are multiples as non-prime/ false
-      if (isPrime[p]) {
-        for (let i = p * p; i <= inputNumber; i += p) {
+      if (isPrime[num]) {
+        for (let i = num * num; i <= inputNumber; i += num) {
           isPrime[i] = false;
         }
       }
